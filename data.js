3c58ee0f28091d6dca2e9cf69660bcf6298d1d66
SyahmiAzhar <syahmi9594@gmail.com>
2021-01-25 19:21:39 +0800

    Maintainence

---
+++ data.js
@@ -0,0 +1,31 @@
+var profile=[
+    {
+        "nama": "Ahmad Syahmi Azhar",
+        "umur": "27",
+        "alamat": "No",
+        "No_Hp": "(+6013-2453897)",
+        "emel": "syahmi9594@gmail.com",
+        "about": "Aku pandai coding"
+    }
+]
+
+var exp=[
+    {
+        "exp1":{
+            "jawatan":"pencacai",
+            "company":"ass tities"
+        },
+        "exp2":{
+            "jawatan":"kuli",
+            "company":"tities"
+        }
+    }
+]
+
+var skill=[
+    
+        "python",
+        "php",
+        "memasak"
+    
+]
